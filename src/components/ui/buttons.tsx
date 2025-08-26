b2059ddf304a8f04224c2fd085992eef6ca3012d
rattes-matheus <matheusrattes8@gmail.com>
2025-08-25 21:53:29 -0300

    create fist vesion off button

---
+++ src/components/ui/buttons.tsx
@@ -0,0 +1,9 @@
+export default function PrimaryButton() {
+  return (
+    <button
+      className="px-4 py-2 rounded-2xl bg-indigo-600 text-white font-semibold hover:-translate-y-0.5 transform hover:shadow-lg hover:-translate-x-0.5"
+    >
+      Clique aqui
+    </button>
+  );
+}
